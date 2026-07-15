ee0ea780409ac7ef2a4b0f7d1a519f0291541e0e
Sina Moradi <sinamoradi002@gmail.com>
2026-07-15 15:36:27 +0330

    Max heap class and its tests completed!

---
+++ src/structures/heap/max_heap.py
@@ -0,0 +1,7 @@
+from src.structures.heap.heap import Heap
+class MaxHeap(Heap):
+    def __init__(self):
+        super().__init__()
+
+    def heapify(self):
+        pass
--- src/tests/max_heap_tst.py
+++ src/tests/max_heap_tst.py
@@ -1,23 +1,23 @@
 import unittest
-from src.structures.heap.min_heap import Min_Heap
+from src.structures.heap.max_heap import MaxHeap
 
 class MyTestCase(unittest.TestCase):
     def setUp(self):
-        self.heap = Min_Heap()
+        self.heap = MaxHeap()
 
     def test_insert(self):
         self.heap.insert(5)
-        self.assertEqual(self.heap.size, 1)
+        self.assertEqual(self.heap.size(), 1)
         self.assertEqual(self.heap.get(0), 5)
         self.heap.insert(10)
-        self.assertEqual(self.heap.size, 2)
-        self.assertEqual(self.heap.get(0), 5)
-        self.assertEqual(self.heap.get(1), 10)
+        self.assertEqual(self.heap.size(), 2)
+        self.assertEqual(self.heap.get(0), 10)
+        self.assertEqual(self.heap.get(1), 5)
         self.heap.insert(1)
-        self.assertEqual(self.heap.size, 3)
-        self.assertEqual(self.heap.get(0), 1)
-        self.assertEqual(self.heap.get(1), 10)
-        self.assertEqual(self.heap.get(2), 5)
+        self.assertEqual(self.heap.size(), 3)
+        self.assertEqual(self.heap.get(0), 10)
+        self.assertEqual(self.heap.get(1), 5)
+        self.assertEqual(self.heap.get(2), 1)
 
     def test_remove(self):
         self.assertIsNone(self.heap.remove())
@@ -26,16 +26,12 @@ class MyTestCase(unittest.TestCase):
         self.heap.insert(1)
         self.heap.insert(2)
         self.heap.insert(8)
-        self.assertEqual(self.heap.size, 5)
-        self.assertEqual(self.heap.get(0), 1)
-        self.heap.remove()
-        self.assertEqual(self.heap.size, 4)
-        self.assertEqual(self.heap.get(0), 2)
-        self.heap.remove()
-        self.assertEqual(self.heap.size, 3)
-        self.assertEqual(self.heap.get(0), 5)
-        self.assertEqual(self.heap.get(1), 8)
-        self.assertEqual(self.heap.get(2), 10)
+        self.assertEqual(self.heap.size(), 5)
+        self.assertEqual(self.heap.get(0), 10)
+        self.test_remove()
+        self.assertEqual(self.heap.get(0), 8)
+        self.assertEqual(self.heap.get(1), 5)
+        self.assertEqual(self.heap.get(2), 1)
 
     def test_decrease_key(self):
         self.heap.insert(11)
@@ -43,17 +39,12 @@ class MyTestCase(unittest.TestCase):
         self.heap.insert(7)
         self.heap.insert(9)
         self.heap.insert(8)
-        self.assertEqual(self.heap.size, 5)
-        self.assertEqual(self.heap.get(0), 7)
-        self.heap.decrease_key(3, 7)
-        self.assertEqual(self.heap.size, 5)
-        self.assertEqual(self.heap.get(0), 4)
-        self.assertEqual(self.heap.get(1), 7)
-        self.assertEqual(self.heap.get(3), 8)
-        self.heap.decrease_key(2 , 2)
-        self.assertEqual(self.heap.size, 5)
-        self.assertEqual(self.heap.get(0), 4)
-        self.assertEqual(self.heap.get(2), 8)
+        self.assertEqual(self.heap.size(), 5)
+        self.assertEqual(self.heap.get(0), 11)
+        self.heap.decrease_key(1 , 5)
+        self.assertEqual(self.heap.size(), 5)
+        self.assertEqual(self.heap.get(1), 9)
+        self.assertEqual(self.heap.get(3), 5)
 
     def test_increase_key(self):
         self.heap.insert(11)
@@ -63,12 +54,10 @@ class MyTestCase(unittest.TestCase):
         self.heap.insert(8)
         self.assertEqual(self.heap.size, 5)
         self.assertEqual(self.heap.get(0), 7)
-        self.heap.increase_key(1, 7)
+        self.heap.increase_key(1 , 2)
         self.assertEqual(self.heap.size, 5)
-        self.assertEqual(self.heap.get(0), 4)
-        self.assertEqual(self.heap.get(1), 9)
-        self.assertEqual(self.heap.get(4), 15)
-        self.assertEqual(self.heap.get(3), 11)
+        self.assertEqual(self.heap.get(1), 11)
+        self.assertEqual(self.heap.get(0), 12)
 
     def test_is_empty(self):
         self.assertTrue(self.heap.is_empty())
@@ -80,20 +69,20 @@ class MyTestCase(unittest.TestCase):
         self.heap.insert(11)
         self.assertEqual(self.heap.peek(), 11)
         self.heap.insert(7)
-        self.assertEqual(self.heap.peek(), 7)
-        self.heap.insert(9)
-        self.assertEqual(self.heap.peek(), 7)
+        self.assertEqual(self.heap.peek(), 11)
+        self.heap.insert(15)
+        self.assertEqual(self.heap.peek(), 15)
 
     def test_parent(self):
         self.assertIsNone(self.heap.parent(0))
         self.heap.insert(11)
         self.assertIsNone(self.heap.parent(0))
         self.heap.insert(7)
-        self.assertEqual(self.heap.parent(1), 7)
+        self.assertEqual(self.heap.parent(1), 11)
         self.assertIsNone(self.heap.parent(0))
         self.heap.insert(9)
-        self.assertEqual(self.heap.parent(1), 7)
-        self.assertEqual(self.heap.parent(2), 7)
+        self.assertEqual(self.heap.parent(1), 11)
+        self.assertEqual(self.heap.parent(2), 11)
         self.assertIsNone(self.heap.parent(0))
 
     def test_right_child(self):
@@ -112,13 +101,11 @@ class MyTestCase(unittest.TestCase):
         self.heap.insert(11)
         self.assertIsNone(self.heap.left_child(0))
         self.heap.insert(7)
-        self.assertEqual(self.heap.left_child(0), 11)
+        self.assertEqual(self.heap.left_child(0), 7)
         self.heap.insert(9)
-        self.assertEqual(self.heap.left_child(0), 11)
+        self.assertEqual(self.heap.left_child(0), 7)
         self.assertIsNone(self.heap.left_child(1))
         self.assertIsNone(self.heap.left_child(2))
-
-
 
 if __name__ == '__main__':
     unittest.main()
